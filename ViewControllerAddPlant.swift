a360adbff963f5bbfd9adb1ec0ba8351569d3ac7
cindyqquach <cq63@drexel.edu>
2023-03-16 04:02:31 -0400

    create branch hw05 + add table

--- ViewControllerAddPlant.swift
+++ ViewControllerAddPlant.swift
@@ -9,16 +9,26 @@ import UIKit
 
 class ViewControllerAddPlant: UIViewController {
 
-    @IBOutlet weak var displayLabel: UILabel!
+
     @IBOutlet weak var nameText: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-    
-    @IBAction func saveButton(_ sender: Any) {
-        displayLabel.text = "Hey there, \(nameText.text!)! Ready to grow with us?"
+  
+    @IBAction func showAlert(_ sender: Any) {
+        let myAlertObj = UIAlertController(title: "Welcome!", message: "Hey \(nameText.text!), ready to grow with us?", preferredStyle: .alert)
+        myAlertObj.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: {
+            (UIAlertAction) in
+            print("No")
+        }))
+        myAlertObj.addAction(UIAlertAction(title: "YES!", style: .default, handler: {
+            (UIAlertAction) in
+            print("Yes")
+        }))
+        myAlertObj.view.tintColor = UIColor(named: "AccentColor")
+        self.present(myAlertObj, animated: true)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
